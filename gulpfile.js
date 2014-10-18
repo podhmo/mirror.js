582517f40e3f7eefd09ba7095896bf29903aeb36
podhmo <podhmo+altair@beproud.jp>
2014-10-19 05:40:36 +0900

    start testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var mocha = require('gulp-mocha');
 
 gulp.task('default', function(){
   gulp.src('./src/*.js')
     .pipe(concat('app.js'))
     .pipe(gulp.dest('./static/js'));
+});
+
+gulp.task('test', function(){
+  gulp.src(['head.js', 'src/*.js', 'test/*.js'])
+    .pipe(mocha({reporter: 'spec'}));
 });
